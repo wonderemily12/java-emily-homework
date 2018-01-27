43b3152e70f82fcde6f9288a8719e1dfb6d07955
Gaeun Kim <wonderemily12@gmail.com>
2018-01-27 13:38:20 +0900

    Homework2

--- src/main/java/codewings/emily/hw4/BigInteger.java
+++ src/main/java/codewings/emily/hw4/BigInteger.java
@@ -1,15 +1,8 @@
 package codewings.emily.hw4;
 
-
 public class BigInteger {
-    private int[] backend = new int[1001];
-    static int flag = 0;
-    static int s = 0;
-    public static int sign() {
-        BigInteger bigint = new BigInteger();
-        if (bigint.backend[0] == "-") s = 1;
-        return s;
-    }
+    public int[] backend = new int[101];
+    private int digit;
 
     public static BigInteger of(String str) {
         BigInteger bigint = new BigInteger();
@@ -20,7 +13,7 @@ public class BigInteger {
 
     public static BigInteger add(BigInteger a, BigInteger b) {
         BigInteger c = new BigInteger();
-        for (int i = 0; i < c.backend.length; i++) {
+        for (int i = 1; i < c.backend.length; i++) {
             c.backend[i] = a.backend[i] + b.backend[i];
         }
         c.adjustCarry();
@@ -29,22 +22,35 @@ public class BigInteger {
 
     public static BigInteger subtract(BigInteger a, BigInteger b) {
         BigInteger c = new BigInteger();
-
-        for (int i = 0; i < c.backend.length; i++) {
-            if (a.backend[i] >= b.backend[i]) {
+        for (int i = c.backend.length - 1; i > 0; i--) {
+            if (a.compareTo(b) == 1 || a.compareTo(b) == 0) {   // a >= b 이면 a - b
                 c.backend[i] = a.backend[i] - b.backend[i];
-                flag = 1;
-            }
-            else {
+            } else if (a.compareTo(b) == -1) {                  // a < b 이면 b - a, 부호 바꿔주기
                 c.backend[i] = b.backend[i] - a.backend[i];
+                c.backend[0] = '-';
             }
         }
         c.adjustBorrow();
         return c;
     }
 
+    public int compareTo(BigInteger other) {
+        for (int i = backend.length - 1; backend[i] == 0; i--) {
+            this.digit = i + 1;
+        }
+        if (this.digit > other.digit) return 1;
+        else if (this.digit == other.digit) {
+            for (int i = this.digit - 1; i >= 0; i--) {
+                if (this.backend[i] > other.backend[i]) return 1;
+                else if (this.backend[i] < other.backend[i]) return -1;
+            }
+            return 0;
+        }
+        else return -1;
+    }
+
     public void adjustCarry() {
-        for (int i = 0; i < backend.length; i++) {
+        for (int i = 1; i < backend.length; i++) {
             while (backend[i] >= 10) {
                 backend[i + 1]++;
                 backend[i] -= 10;
@@ -53,7 +59,7 @@ public class BigInteger {
     }
 
     public void adjustBorrow() {
-        for (int i = 0; i < backend.length; i++) {
+        for (int i = 1; i < backend.length; i++) {
             while (backend[i] < 0) {
                 backend[i + 1]--;
                 backend[i] += 10;
@@ -63,22 +69,15 @@ public class BigInteger {
 
     public void print() {
         int j = 0;
-        int z = 0;
-        for (int i = backend.length - 1; backend[i] == 0; i--) {
-            j = i - 1;
+        for (int i = backend.length - 1; backend[i] == 0; i--) { // backend 배열에 0밖에 없어서 break 불가능.
+            if (backend[i] != 0) {
+                j = i;
+                break;
+            }
         }
         while (j >= 0) {
-            if (flag == 0) System.out.print(backend[j]);
-            else {
-                if (z == 0) {
-                    System.out.print("-");
-                    z = 1;
-                }
-                System.out.print(backend[j]);
-            }
+            System.out.print(backend[j]);
             j--;
         }
-        System.out.println();
     }
 }
-
--- src/test/java/codewings/emily/hw4/BigIntegerMain.java
+++ src/test/java/codewings/emily/hw4/BigIntegerMain.java
@@ -1,22 +1,43 @@
 package codewings.emily.hw4;
 
+import com.intellij.icons.AllIcons;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BigIntegerMain {
     public static void main(String[] args) throws IOException {
+//        testCompareTo();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             BigInteger a = BigInteger.of(reader.readLine());
             BigInteger b = BigInteger.of(reader.readLine());
-            BigInteger c;
-            if (BigInteger.sign() == 0) {
-                c = BigInteger.add(a, b);
-            }
-            else  {
-                c = BigInteger.subtract(a, b);
+            BigInteger c = new BigInteger();
+            for (int i = 0; i < c.backend.length; i++) {
+                if (a.backend[0] == '+' && b.backend[0] == '+') {
+                    c = BigInteger.add(a, b);
+                }
+                else if (a.backend[0] == '+' && b.backend[0] == '-' ||  a.backend[0] == '-' && b.backend[0] == '+') {
+                    c = BigInteger.subtract(a, b);
+                }
+                else if (a.backend[0] == '-' && b.backend[0] == '-') {
+                    c = BigInteger.add(a, b);
+                    c.backend[0] = '-';
+                }
             }
             c.print();
         }
     }
+
+//    public static void testCompareTo() {
+//        assert BigInteger.of("123").compareTo(BigInteger.of("45")) > 0;
+//        assert BigInteger.of("45").compareTo(BigInteger.of("123")) < 0;
+//        assert BigInteger.of("12345").compareTo(BigInteger.of("12346")) < 0;
+//        assert BigInteger.of("12345").compareTo(BigInteger.of("12344")) > 0;
+//        assert BigInteger.of("123").compareTo(BigInteger.of("123")) == 0;
+//        System.out.println("Test Succeed");
+//    }
 }
+
+
+
